b1e5021b78e7c1fd7d5ef9751a2e93e0622d1a97
Alperen Aktuğ <aktugalperen9@gmail.com>
2024-05-01 15:22:31 +0300

    use of redux-toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { useSelector, useDispatch } from "react-redux";
+import { store } from "./redux/store";
+import { decrement, increment } from "./redux/counterSlice";
+
 const App = () => {
-  return <div>App</div>;
+  const dispatch = useDispatch();
+
+  const { value } = useSelector((store) => store.counter);
+  console.log(value);
+  return (
+    <div>
+      <div>{value}</div>
+      <div>
+        <button onClick={() => dispatch(increment())}>Arttır</button>{" "}
+        <button onClick={() => dispatch(decrement())}>Azalt</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
